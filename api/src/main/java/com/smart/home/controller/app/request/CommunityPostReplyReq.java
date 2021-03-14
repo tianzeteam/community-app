d0e5b721e679c5dda249924f2923a55842707d63
297383949@qq.com <storm880521>
2021-03-14 16:48:16 +0800

    bugfix

--- api/src/main/java/com/smart/home/controller/app/request/CommunityPostReplyReq.java
+++ api/src/main/java/com/smart/home/controller/app/request/CommunityPostReplyReq.java
@@ -4,16 +4,23 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @ApiModel("帖子评论params")
 @Data
 public class CommunityPostReplyReq {
 
     @ApiModelProperty("帖子id")
+    @NotNull(message = "帖子id不能为空")
     private Long id;
 
     @ApiModelProperty("帖子作者id")
+    @NotNull(message = "帖子作者id不能为空")
     private Long authorId;
 
     @ApiModelProperty("评论内容")
+    @NotNull(message = "评论内容不能为空")
+    @Size(max = 200, message = "评论内容不能超过200字")
     private String contents;
 }
