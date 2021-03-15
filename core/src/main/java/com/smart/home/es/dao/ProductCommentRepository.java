5effb4f44302ea70e5304cb0cd73759caeebc3ee
297383949@qq.com <storm880521>
2021-03-15 11:56:08 +0800

    es 文章bean

--- core/src/main/java/com/smart/home/es/dao/ProductCommentRepository.java
+++ core/src/main/java/com/smart/home/es/dao/ProductCommentRepository.java
@@ -2,10 +2,12 @@ package com.smart.home.es.dao;
 
 import com.smart.home.es.bean.ProductCommentBean;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author jason
  * @date 2021/3/8
  **/
+@Repository
 public interface ProductCommentRepository extends ElasticsearchRepository<ProductCommentBean, Long> {
 }
--- core/src/main/java/com/smart/home/es/dao/SearchKeyRepository.java
+++ core/src/main/java/com/smart/home/es/dao/SearchKeyRepository.java
@@ -2,6 +2,8 @@ package com.smart.home.es.dao;
 
 import com.smart.home.es.bean.SearchKey;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SearchKeyRepository extends ElasticsearchRepository<SearchKey, String> {
 }
