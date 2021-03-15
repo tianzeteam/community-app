a8bcad4994b96f7465e7960762b22bf19347fdb5
297383949@qq.com <storm880521>
2021-03-15 20:33:02 +0800

    编译不通过bugfix

--- core/src/main/java/com/smart/home/es/dto/EsSearchDTO.java
+++ core/src/main/java/com/smart/home/es/dto/EsSearchDTO.java
@@ -1,12 +1,16 @@
 package com.smart.home.es.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class EsSearchDTO {
 
--- core/src/main/java/com/smart/home/es/dto/ProductEsDTO.java
+++ core/src/main/java/com/smart/home/es/dto/ProductEsDTO.java
@@ -1,10 +1,13 @@
 package com.smart.home.es.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductEsDTO extends EsSearchDTO{
 
     private String productName;
