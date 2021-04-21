9beb06ae331ae374b54e75db3fdc2a764b84d130
297383949@qq.com <storm880521>
2021-04-21 12:59:34 +0800

    bugfix

--- api/src/main/java/com/smart/home/controller/pc/request/system/SysActionLogSearchDTO.java
+++ api/src/main/java/com/smart/home/controller/pc/request/system/SysActionLogSearchDTO.java
@@ -1,5 +1,6 @@
 package com.smart.home.controller.pc.request.system;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.smart.home.dto.RequestPageBean;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -19,9 +20,12 @@ public class SysActionLogSearchDTO extends RequestPageBean {
     @ApiModelProperty("操作人主键id")
     private Long userId;
     @ApiModelProperty("开始时间")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private Date startDate;
     @ApiModelProperty("结束时间")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private Date endDate;
 
 }
