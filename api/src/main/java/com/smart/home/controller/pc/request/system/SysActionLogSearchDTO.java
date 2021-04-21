19373a35c6c1b84c242db181b99b9d115a0e4a7f
297383949@qq.com <storm880521>
2021-04-21 11:47:50 +0800

    bugfix

--- api/src/main/java/com/smart/home/controller/pc/request/system/SysActionLogSearchDTO.java
+++ api/src/main/java/com/smart/home/controller/pc/request/system/SysActionLogSearchDTO.java
@@ -4,6 +4,7 @@ import com.smart.home.dto.RequestPageBean;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -20,6 +21,7 @@ public class SysActionLogSearchDTO extends RequestPageBean {
     @ApiModelProperty("开始时间")
     private Date startDate;
     @ApiModelProperty("结束时间")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date endDate;
 
 }
